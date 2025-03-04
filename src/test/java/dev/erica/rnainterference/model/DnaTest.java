45cf35bb1d40294aaefba79db137f6f9fd214871
Erica <you@example.com>
2025-03-04 21:16:20 +0100

    test getSequence done. TDD: ❌

---
+++ src/test/java/dev/erica/rnainterference/model/DnaTest.java
@@ -0,0 +1,16 @@
+package dev.erica.rnainterference.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DnaTest {
+    @Test
+    @DisplayName("Testo para obtener la secuencia de ADN")
+    public void getSequence() {
+        DNA sequence = new DNA("GCTA");
+
+        assertEquals("GCTA", sequence.getSequence());
+    }
+}
