0f06e5a7e68a2243ea88a08f1726a566b4a05c98
erikamc99 <erikamc99@gmail.com>
2025-03-07 14:26:07 +0100

    interface implemented and override created. TDD: ✔️

--- src/main/java/dev/erica/rnainterference/model/DnaTranscriber.java
+++ src/main/java/dev/erica/rnainterference/model/DnaTranscriber.java
@@ -2,12 +2,24 @@ package dev.erica.rnainterference.model;
 
 import java.util.Map;
 
-public class DnaTranscriber {
+public class DnaTranscriber implements Transcriber {
     private static final Map<Character, Character> TRANSCRIPTION_MAP = Map.of(
         'G', 'C', 'C', 'G', 'T', 'A', 'A', 'U'
     );
 
     public static Map<Character, Character> getTranscriptionMap() {
         return TRANSCRIPTION_MAP;
+    }
+
+    @Override
+    public String transcribe(DNA sequence) {
+        
+        StringBuilder rna = new StringBuilder();
+        
+        for (char nucleotide : sequence.getSequence().toUpperCase().toCharArray()) {
+            rna.append(TRANSCRIPTION_MAP.getOrDefault(nucleotide, '?'));
+        }
+        
+        return rna.toString();
     }
 }
